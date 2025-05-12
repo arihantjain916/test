30aeb87c1f19dd87734ac5351431be0420b04ee9
Arihant Jain <arihantj916@gmail.com>
2025-05-12 17:27:07 +0530

    feat: add api endpoint to send email messages

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import morgan from "morgan";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-import axios from "axios";
 import cors from "cors";
 import multer from "multer";
+import nodemailer from "nodemailer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -128,38 +128,57 @@ app.get("/clear", (req, res) => {
   }
 });
 
-// app.get("/getData", async (req, res) => {
-//   const fileUrl = "http://localhost:3000/data";
-//   //   const clearUrl = "https://celitix.com/landing/clear-submissions.php"; // We’ll create this
+app.post("/send-email", (req, res) => {
+  const {
+    name,
+    email,
+    phone,
+    company,
+    service,
+    message,
+    source = "website",
+    designation,
+    experience,
+    resume
+  } = req.body;
 
-//   const response = await axios.get(fileUrl);
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "info@proactivedigital.in",
+      pass: "womb nmot zyfb yqfp",
+    },
+  });
 
-//   const content = response?.data?.trim();
+  var mailOptions = {};
+  if (source === "careers") {
+    mailOptions = {
+      from: "ads@proactivedigital.in",
+      to: "info@proactivedigital.in, sales@proactivesms.in, yogendra@proactivesms.in, arihantj916@gmail.com",
+      subject: "Celitix Carrers Enquiry",
+      html: `Name: ${name}<br>Email: ${email}<br>Phone: ${phone}<br>Company: ${company}<br>Service: ${service}<br>Message: ${message}<br>Designation: ${designation}<br>Experience: ${experience}<br>Resume: ${resume}`,
+    };
+  } else {
+    mailOptions = {
+      from: "ads@proactivedigital.in",
+      to: "info@proactivedigital.in, sales@proactivesms.in, yogendra@proactivesms.in, arihantj916@gmail.com",
+      subject: "Celitix Contact Enquiry",
+      html: `Name: ${name}<br>Email: ${email}<br>Phone: ${phone}<br>Company: ${company}<br>Service: ${service}<br>Message: ${message}`,
+    };
+  }
 
-//   if (!content) {
-//     return res.status(400).json({
-//       message: "No new data",
-//       status: false,
-//     });
-//   }
-
-//   const lines = content.split("\n");
-//   const dataSheet = [];
-
-//   lines.forEach((line) => {
-//     const data = line.split(",");
-//     if (data.length >= 6) {
-//       dataSheet.push(data);
-//     }
-//   });
-
-//   return res.status(200).json({
-//     message: dataSheet,
-//     status: true,
-//   });
-// });
-
-// app.post("")
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+  return res.status(200).json({
+    message: "Email sent successfully",
+    status: true,
+  });
+});
 
 const port = 3000;
 
